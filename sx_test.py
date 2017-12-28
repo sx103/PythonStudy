bf48869f5fba08f95ba99074bed436a24464917e
sx103 <kenneth.shu@139.com>
2017-12-28 23:39:45 +0800

    Remove lines that are not used.

--- sx_test.py
+++ sx_test.py
@@ -9,5 +9,5 @@ print(dir())
 print("Invoke saySth function.")
 test_module_2.saySth()
 
-print("Adding a test function here.")
-print("Adding another test function here")
+#print("Adding a test function here.")
+#print("Adding another test function here")
